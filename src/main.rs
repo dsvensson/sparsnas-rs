1142d5550d0b212146685656a4f8a725a837f39b
Daniel Svensson <dsvensson@gmail.com>
2018-04-21 07:58:15 +0000

    Decode packet and start to build some kind of application.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
+extern crate byteorder;
 extern crate cc1101;
 extern crate linux_embedded_hal as hal;
 
-use hal::{Pin, Spidev};
+use byteorder::{BigEndian, ReadBytesExt};
 use hal::spidev::SpidevOptions;
 use hal::sysfs_gpio::Direction;
+use hal::{Pin, Spidev};
 
-use std::{thread, time};
 use std::fmt::Write;
+use std::io::{Error, Read};
+use std::{thread, time};
 
 use cc1101::{Cc1101, Modulation, PacketMode, RadioMode};
+
+pub struct IterReader<I>(pub I);
+
+impl<'a, I: Iterator<Item = u8>> Read for IterReader<I> {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        let mut count: usize = 0;
+        for i in 0..buf.len() {
+            match self.0.next() {
+                Some(v) => {
+                    buf[i] = v;
+                    count += 1;
+                }
+                None => break,
+            }
+        }
+        Ok(count)
+    }
+}
 
 fn dump(buf: &[u8]) -> String {
     let mut s = String::new();
@@ -18,17 +39,8 @@ fn dump(buf: &[u8]) -> String {
     s
 }
 
-fn main() {
-    let mut spi = Spidev::open("/dev/spidev0.0").expect("SPI open error");
-    let options = SpidevOptions::new().max_speed_hz(50_000).build();
-    spi.configure(&options).expect("SPI configure error");
-
-    let cs = Pin::new(24);
-    cs.export().unwrap();
-    while !cs.is_exported() {}
-    cs.set_direction(Direction::Out).unwrap();
-
-    let mut cc1101 = Cc1101::new(spi, cs).unwrap();
+fn configure_radio(spi: Spidev, cs: Pin) -> Result<Cc1101<Spidev, Pin>, Error> {
+    let mut cc1101 = Cc1101::new(spi, cs)?;
 
     cc1101.reset().expect("Reset failed");
 
@@ -55,45 +67,85 @@ fn main() {
         .expect("Setting packet mode failed");
 
     cc1101
-        .set_packet_length(0x11)
+        .set_packet_length(20)
         .expect("Setting packet length failed");
 
-    loop {
-        // Should use GDO2 to check packet status, then there will be no
-        // underflows and radio will remain in receive mode.
+    Ok(cc1101)
+}
 
-        cc1101
-            .set_radio_mode(RadioMode::Receive)
-            .expect("Enabling reception failed");
+fn receive_packet(cc1101: &mut Cc1101<Spidev, Pin>) -> Result<(), Error> {
+    cc1101
+        .set_radio_mode(RadioMode::Receive)
+        .expect("Enabling reception failed");
 
-        thread::sleep(time::Duration::from_millis(10));
+    thread::sleep(time::Duration::from_millis(10));
 
-        let mut frame = [0u8; 0x21];
-        let mut rssi = 0u8;
-        let mut lqi = 0u8;
-        cc1101.receive(&mut frame, &mut rssi, &mut lqi).unwrap();
+    let mut frame = [0u8; 21];
+    let mut rssi = 0u8;
+    let mut lqi = 0u8;
 
-        let buf = &frame[1..];
-        let len = buf[0];
-        let addr = buf[1];
+    cc1101.receive(&mut frame, &mut rssi, &mut lqi)?;
 
-        if addr != 0x3e {
-            continue;
-        }
+    let buf = &frame[1..];
+    let len = buf[0];
+    let addr = buf[1];
 
-        let payload = &buf[2..18];
-        let crc = (buf[18] as u16) << 8 | buf[19] as u16;
+    let seq = buf[2];
+    let payload = &buf[3..18];
+
+    println!(
+        "len: {:02x} addr: {:02x} data: {} len: {}, ok: {}, {:02x} {:02x}",
+        len,
+        addr,
+        dump(payload),
+        payload.len(),
+        (lqi & 0b10000000) > 0,
+        rssi,
+        lqi,
+    );
+
+    // Should fix CRC again... and probably check some package characteristics.
+    if len == 0x11 && addr == 0x3e {
+        let mut dec = IterReader(
+            payload
+                .iter()
+                .zip([0x47, 0xd0, 0xa2, 0x73, 0x80].iter().cycle())
+                .map(|(p, k)| p ^ k),
+        );
+
+        let status = dec.read_u16::<BigEndian>().unwrap();
+        let fixed = dec.read_u32::<BigEndian>().unwrap();
+        let pcnt = dec.read_u16::<BigEndian>().unwrap();
+        let avg = dec.read_u16::<BigEndian>().unwrap();
+        let count = dec.read_u32::<BigEndian>().unwrap();
+        let unknown = dec.read_u8().unwrap();
 
         println!(
-            "len: {:02x} addr: {:02x} data: {} crc: {:04x} len: {}, ok: {}, {:02x} {:02x}",
-            len,
-            addr,
-            dump(payload),
-            crc,
-            payload.len(),
-            (lqi & 0b10000000) > 0,
-            rssi,
-            lqi,
+            " {:02x} {:02x} {:02x}  {:04x}   {:08x} {:04x} {:04x}    {:08x} {:02x} # Current power: {}",
+            len, addr, seq, status, fixed, pcnt, avg, count, unknown, 3686400 / avg as u32
         );
     }
+
+    Ok(())
+}
+
+fn run() -> Result<(), Error> {
+    let mut spi = Spidev::open("/dev/spidev0.0").expect("Could not open SPI device");
+    let options = SpidevOptions::new().max_speed_hz(50_000).build();
+    spi.configure(&options).expect("SPI configure error");
+
+    let cs = Pin::new(24);
+    cs.export().unwrap();
+    while !cs.is_exported() {}
+    cs.set_direction(Direction::Out).unwrap();
+
+    let mut cc1101 = configure_radio(spi, cs)?;
+
+    loop {
+        receive_packet(&mut cc1101)?;
+    }
+}
+
+fn main() {
+    run().unwrap();
 }
